fc6b14d165451e93064dc15993a7e9418531f648
smo93 <georgimpeev@gmail.com>
2014-03-08 15:01:27 +0200

    "Added solution for count_words"

---
+++ week0/count_words/solution.py
@@ -0,0 +1,8 @@
+def count_words(arr):
+    result = {}
+    for word in arr:
+        if word in result:
+            result[word] += 1
+        else:
+            result[word] = 1
+    return result
