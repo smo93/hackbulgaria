487f6460e7e875f0a987bae18552d365d338f92f
smo93 <georgimpeev@gmail.com>
2014-03-16 22:51:12 +0200

    Added unittest for the cat2 problem.

---
+++ week0/cat2/test.py
@@ -0,0 +1,22 @@
+import unittest
+import solution
+from subprocess import call, check_output
+
+
+class Cat2Test(unittest.TestCase):
+    """docstring for Cat2Test"""
+    def setUp(self):
+        call('echo \'File1\' > file1', shell=True)
+        call('echo \'File2\' > file2', shell=True)
+
+    def test_cat2(self):
+        expexted_output = 'File1\n\nFile2\n\n'
+        output = check_output('py solution.py file1 file2', shell=True)
+        output = output.decode()
+        self.assertEqual(expexted_output, output)
+
+    def tearDown(self):
+        call('rm file1 file1', shell=True)
+
+if __name__ == '__main__':
+    unittest.main()
