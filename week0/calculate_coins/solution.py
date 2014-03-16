7ffeafe2e8299cab1047ac1224e2088e486d1233
smo93 <georgimpeev@gmail.com>
2014-03-16 16:42:27 +0200

    Added some minor changes.

--- week0/calculate_coins/solution.py
+++ week0/calculate_coins/solution.py
@@ -1,10 +1,13 @@
 def calculate_coins(sum):
-	coins = [100,50,20,10,5,2,1]
-	result = {}
-	for coin in coins:
-		count = 0
-		while sum - coin / 100 >= 0:
-			count += 1
-			sum -= coin / 100
-		result[coin] = count
-	return result
+    if sum < 0:
+        return False
+    sum *= 100
+    coins = [100,50,20,10,5,2,1]
+    result = {}
+    for coin in coins:
+            count = 0
+            while sum - coin >= 0:
+                    count += 1
+                    sum -= coin
+            result[coin] = count
+    return result
