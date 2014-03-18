e802fc575fa0d0de3b17f50d9d3ec1dd794633cf
smo93 <georgimpeev@gmail.com>
2014-03-18 12:23:53 +0200

    Fixed the indentation.

--- week0/magic_string/solution.py
+++ week0/magic_string/solution.py
@@ -1,7 +1,12 @@
 def magic_string(string):
-	count = 0
-	for i in range(int(len(string) / 2)):
-		if string[i] != '>':
-			string[i] = '>'
-			count += 1
-	
+    string = list(string)
+    count = 0
+    for i in range(int(len(string) / 2)):
+        if string[i] != '>':
+            string[i] = '>'
+            count += 1
+        if string[-(i + 1)] != '<':
+            string[-(i + 1)] = '>'
+            count += 1
+    return count
+
