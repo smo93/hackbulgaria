e8d62c9fe416bd68f40371f185cc83c5166b3cd1
smo93 <georgimpeev@gmail.com>
2014-03-19 11:59:46 +0200

    is_prime func added.

--- week0/prime_factorization/solution.py
+++ week0/prime_factorization/solution.py
@@ -1,13 +1,22 @@
+from math import sqrt
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
 def prime_factorization(n):
-	i = 2
-	res = []
-	while n != 1:
-		count = 0;
-		if is_prime(i):
-			while n % i == 0:
-				count += 1
-				n /= i
-			if count != 0:
-				res.append((i, count))
-		i += 1
-	return res
+    i = 2
+    res = []
+    while n != 1:
+        count = 0;
+        if is_prime(i):
+            while n % i == 0:
+                count += 1
+                n /= i
+            if count != 0:
+                res.append((i, count))
+        i += 1
+    return res
