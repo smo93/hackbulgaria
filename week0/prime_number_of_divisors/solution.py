c8e2fddddf9db0e8b55642ce435611b6ec4f3eb6
smo93 <georgimpeev@gmail.com>
2014-03-19 12:48:04 +0200

    is_prime func added.

--- week0/prime_number_of_divisors/solution.py
+++ week0/prime_number_of_divisors/solution.py
@@ -1,3 +1,13 @@
+from math import sqrt
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
 def prime_number_of_divisors(n):
 	number_of_divisors = 0
 	for i in range(1, n + 1):
