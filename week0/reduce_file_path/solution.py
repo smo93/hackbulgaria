e7674335ac851add8206f318f999f7cfa8a40c44
smo93 <georgimpeev@gmail.com>
2014-03-19 13:41:33 +0200

    minor fix

--- week0/reduce_file_path/solution.py
+++ week0/reduce_file_path/solution.py
@@ -1,9 +1,9 @@
 import re
 
 def reduce_file_path(path):
-    new_path = re.sub('\w+/\.\./', '/', path)
+    new_path = re.sub('/\./', '/', path)
+    new_path = re.sub('\w*/{0,1}\.\./', '/', new_path)
     new_path = re.sub('/\.\./', '/', new_path)
-    new_path = re.sub('\./', '/', new_path)
     new_path = re.sub('//+', '/', new_path)
     if new_path[-1] == '/' and new_path != '/':
         return new_path[:-1]
