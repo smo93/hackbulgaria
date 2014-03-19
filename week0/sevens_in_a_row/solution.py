48304206a7eaae4db774979b9156477053038159
smo93 <georgimpeev@gmail.com>
2014-03-19 13:50:33 +0200

    fixed the indentation

--- week0/sevens_in_a_row/solution.py
+++ week0/sevens_in_a_row/solution.py
@@ -1,10 +1,12 @@
 def sevens_in_a_row(arr, n):
-	count = 0
-	for el in arr:
-		if el == 7:
-			count += 1
-		else:
-			count = 0
-		if count == n:
-			return True
-	return False
+    count = 0
+    for el in arr:
+        if el == 7:
+            count += 1
+            if count == n:
+                return True
+        else:
+            count = 0
+    if count == n:
+        return True
+    return False
