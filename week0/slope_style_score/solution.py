eed3ae282db2671a8ebf9275ab3483f772438901
smo93 <georgimpeev@gmail.com>
2014-03-19 14:17:23 +0200

    fixed the indentation

--- week0/slope_style_score/solution.py
+++ week0/slope_style_score/solution.py
@@ -1,6 +1,6 @@
 def slope_style_score(scores):
-	scores.sort()
-	total = 0
-	for i in scores[1:-1]:
-		total += i
-	return total / (len(scores) - 2)
+    scores.sort()
+    total = 0
+    for i in scores[1:-1]:
+        total += i
+    return int(total / (len(scores) - 2) * 100) / 100
