6bfd1bd2ed5c3a5ddb6bbd0bd235e0f63bc5c45e
smo93 <georgimpeev@gmail.com>
2014-03-21 23:07:11 +0200

    .

--- week0/unique_words_count/solution.py
+++ week0/unique_words_count/solution.py
@@ -1,2 +1,11 @@
+def count_words(arr):
+    result = {}
+    for word in arr:
+        if word in result:
+            result[word] += 1
+        else:
+            result[word] = 1
+    return result
+
 def unique_words_count(arr):
     return len(count_words(arr))
