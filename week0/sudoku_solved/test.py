58a65cf9d99670ce93e890f4b975d4598af1058d
smo93 <georgimpeev@gmail.com>
2014-03-22 13:01:46 +0200

    added unittests for the sudoku_solved problem

---
+++ week0/sudoku_solved/test.py
@@ -0,0 +1,31 @@
+from solution import sudoku_solved
+import unittest
+
+
+class SudokuSolvedTest(unittest.TestCase):
+    """docstring for SudokuSolvedTest"""
+    def test_sudoku_solved(self):
+        solved = [[4, 5, 2, 3, 8, 9, 7, 1, 6],
+            [3, 8, 7, 4, 6, 1, 2, 9, 5],
+            [6, 1, 9, 2, 5, 7, 3, 4 ,8],
+            [9, 3, 5, 1, 2, 6, 8, 7, 4],
+            [7, 6, 4, 9, 3, 8, 5, 2, 1],
+            [1, 2, 8, 5, 7, 4, 6, 3, 9],
+            [5, 7, 1, 8, 9, 2, 4, 6, 3],
+            [8, 9, 6, 7, 4, 3, 1, 5 ,2],
+            [2, 4, 3, 6, 1, 5, 9, 8, 7]]
+        self.assertTrue(sudoku_solved(solved))
+
+        wrong = [[1, 2, 3, 4, 5, 6, 7, 8, 9],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9]]
+        self.assertFalse(sudoku_solved(wrong))
+
+if __name__ == '__main__':
+    unittest.main()
